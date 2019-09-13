09d84a12ae39f7f1362a8acfde0733dbe1719fb0
Karl Tauber <karl@jformdesigner.com>
2019-09-13 13:12:05 +0200

    List: use inactive selection background/foreground if list is not focused

--- flatlaf-core/src/main/java/com/formdev/flatlaf/ui/FlatListUI.java
+++ flatlaf-core/src/main/java/com/formdev/flatlaf/ui/FlatListUI.java
@@ -16,7 +16,15 @@
 
 package com.formdev.flatlaf.ui;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import javax.swing.JComponent;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.UIManager;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicListUI;
 
@@ -25,12 +33,64 @@ import javax.swing.plaf.basic.BasicListUI;
  *
  * TODO document used UI defaults of superclass
  *
+ * @uiDefault List.selectionInactiveBackground		Color
+ * @uiDefault List.selectionInactiveForeground		Color
+ *
  * @author Karl Tauber
  */
 public class FlatListUI
 	extends BasicListUI
 {
+	protected Color selectionInactiveBackground;
+	protected Color selectionInactiveForeground;
+
 	public static ComponentUI createUI( JComponent c ) {
 		return new FlatListUI();
+	}
+
+	@Override
+	protected void installDefaults() {
+		super.installDefaults();
+
+		selectionInactiveBackground = UIManager.getColor( "List.selectionInactiveBackground" );
+		selectionInactiveForeground = UIManager.getColor( "List.selectionInactiveForeground" );
+	}
+
+	@Override
+	protected void uninstallDefaults() {
+		super.uninstallDefaults();
+
+		selectionInactiveBackground = null;
+		selectionInactiveForeground = null;
+	}
+
+	/**
+	 * Same as super.paintCell(), but uses inactive selection background/foreground if list is not focused.
+	 */
+	@Override
+	@SuppressWarnings( { "rawtypes", "unchecked" } )
+	protected void paintCell( Graphics g, int row, Rectangle rowBounds, ListCellRenderer cellRenderer,
+		ListModel dataModel, ListSelectionModel selModel, int leadIndex )
+	{
+		Object value = dataModel.getElementAt( row );
+		boolean hasFocus = list.hasFocus();
+		boolean cellHasFocus = hasFocus && (row == leadIndex);
+		boolean isSelected = selModel.isSelectedIndex( row );
+
+		// get renderer component
+		Component rendererComponent = cellRenderer.getListCellRendererComponent(
+			list, value, row, isSelected, cellHasFocus );
+
+		// apply inactive selection background/foreground if list is not focused
+		if( isSelected && !hasFocus ) {
+			if( rendererComponent.getBackground() == list.getSelectionBackground() )
+				rendererComponent.setBackground( selectionInactiveBackground );
+			if( rendererComponent.getForeground() == list.getSelectionForeground() )
+				rendererComponent.setForeground( selectionInactiveForeground );
+		}
+
+		// paint renderer
+		rendererPane.paintComponent( g, rendererComponent, list,
+			rowBounds.x, rowBounds.y, rowBounds.width, rowBounds.height, true );
 	}
 }
