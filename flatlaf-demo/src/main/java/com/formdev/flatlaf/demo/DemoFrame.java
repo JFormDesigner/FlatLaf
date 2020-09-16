JFormDesigner/FlatLaf

commit ee6a1da70981ccbb991a93ff23f481dfdb49227c

Demo: exit even if UI defaults inspector window is shown

flatlaf-demo/src/main/java/com/formdev/flatlaf/demo/DemoFrame.java
@@ -69,6 +69,13 @@ class DemoFrame
 		} );
 	}
 
+	@Override
+	public void dispose() {
+		super.dispose();
+
+		System.exit( 0 );
+	}
+
 	private void showHints() {
 		Hint fontMenuHint = new Hint(
 			"Use 'Font' menu to increase/decrease font size or try different fonts.",
@@ -109,7 +116,7 @@ class DemoFrame
 	}
 
 	private void exitActionPerformed() {
-		System.exit( 0 );
+		dispose();
 	}
 
 	private void aboutActionPerformed() {
@@ -337,7 +344,7 @@ class DemoFrame
 
 		//======== this ========
 		setTitle("FlatLaf Demo");
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
 

flatlaf-testing/src/main/java/com/formdev/flatlaf/testing/FlatTestFrame.java
@@ -253,6 +253,13 @@ public class FlatTestFrame
 		} );
 	}
 
+	@Override
+	public void dispose() {
+		super.dispose();
+
+		System.exit( 0 );
+	}
+
 	private void updateTitle() {
 		double systemScaleFactor = UIScale.getSystemScaleFactor( getGraphicsConfiguration() );
 		float userScaleFactor = UIScale.getUserScaleFactor();
@@ -637,7 +644,7 @@ public class FlatTestFrame
 		themesPanel = new IJThemesPanel();
 
 		//======== this ========
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
 

flatlaf-demo/src/main/java/com/formdev/flatlaf/demo/DemoFrame.jfd
@@ -9,7 +9,7 @@ new FormModel {
 		add( new FormWindow( "javax.swing.JFrame", new FormLayoutManager( class java.awt.BorderLayout ) ) {
 			name: "this"
 			"title": "FlatLaf Demo"
-			"defaultCloseOperation": 3
+			"defaultCloseOperation": 2
 			"$locationPolicy": 2
 			"$sizePolicy": 2
 			add( new FormContainer( "javax.swing.JToolBar", new FormLayoutManager( class javax.swing.JToolBar ) ) {

flatlaf-testing/src/main/java/com/formdev/flatlaf/testing/FlatTestFrame.jfd
@@ -7,7 +7,7 @@ new FormModel {
 			name: "this"
 			"$locationPolicy": 2
 			"$sizePolicy": 2
-			"defaultCloseOperation": 3
+			"defaultCloseOperation": 2
 			add( new FormContainer( "javax.swing.JPanel", new FormLayoutManager( class java.awt.BorderLayout ) ) {
 				name: "dialogPane"
 				add( new FormContainer( "javax.swing.JPanel", new FormLayoutManager( class net.miginfocom.swing.MigLayout ) {

commit 44d196fb8cc170683c21d64ca7d8caee3850121d

Demo: menu item "Options > Window decorations" did exit Demo (regression in commit ee6a1da70981ccbb991a93ff23f481dfdb49227c)

flatlaf-demo/src/main/java/com/formdev/flatlaf/demo/DemoFrame.java
@@ -73,7 +73,7 @@ class DemoFrame
 	public void dispose() {
 		super.dispose();
 
-		System.exit( 0 );
+		FlatUIDefaultsInspector.hide();
 	}
 
 	private void showHints() {

flatlaf-testing/src/main/java/com/formdev/flatlaf/testing/FlatTestFrame.java
@@ -257,7 +257,7 @@ public class FlatTestFrame
 	public void dispose() {
 		super.dispose();
 
-		System.exit( 0 );
+		FlatUIDefaultsInspector.hide();
 	}
 
 	private void updateTitle() {