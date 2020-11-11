2d49d349037b148fb3627542f41a4c7ecec8520b
mmatessi <17149962+basix86@users.noreply.github.com>
2020-11-11 16:37:54 +0100

    rangeSliderUi test 4

--- flatlaf-testing/src/main/java/com/formdev/flatlaf/testing/jideoss/FlatRangeSliderTest.java
+++ flatlaf-testing/src/main/java/com/formdev/flatlaf/testing/jideoss/FlatRangeSliderTest.java
@@ -1,34 +1,28 @@
 package com.formdev.flatlaf.testing.jideoss;
 
-import com.formdev.flatlaf.testing.FlatTestFrame;
-import com.formdev.flatlaf.testing.FlatTestPanel;
-import com.jgoodies.forms.factories.*;
-import com.jgoodies.forms.layout.CellConstraints;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jidesoft.plaf.LookAndFeelFactory;
-import com.jidesoft.popup.JidePopup;
-import com.jidesoft.swing.*;
-import com.jidesoft.swing.JideTabbedPane;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JButton;
+import java.awt.event.*;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import static com.formdev.flatlaf.FlatClientProperties.TABBED_PANE_HAS_FULL_BORDER;
+import javax.swing.event.*;
+import com.formdev.flatlaf.testing.FlatTestFrame;
+import com.formdev.flatlaf.testing.FlatTestPanel;
+import com.jgoodies.forms.factories.CC;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jidesoft.plaf.LookAndFeelFactory;
+import com.jidesoft.swing.RangeSlider;
+import net.miginfocom.swing.MigLayout;
 
 public class FlatRangeSliderTest
 	extends FlatTestPanel
 {
+
+	private RangeSlider horizontalRangeSlider;
+	private RangeSlider verticalRangeSlider;
+
 	public static void main( String[] args ) {
 		SwingUtilities.invokeLater( () -> {
 			FlatTestFrame frame = FlatTestFrame.create( args, "FlatRangeSliderTest" );
@@ -47,42 +41,14 @@ public class FlatRangeSliderTest
 		initComponents();
 	}
 
-
-	private void hasFullBorderChanged() {
-		Boolean hasFullBorder = hasFullBorderCheckBox.isSelected() ? true : null;
-//		tabbedPane1.putClientProperty( TABBED_PANE_HAS_FULL_BORDER, hasFullBorder );
+	private void paintLabels() {
+		horizontalRangeSlider.setPaintLabels( paintLabel.isSelected() );
+		verticalRangeSlider.setPaintLabels( paintLabel.isSelected() );
 	}
 
-	private void moreTabsChanged() {
-		boolean moreTabs = moreTabsCheckBox.isSelected();
-//		addRemoveMoreTabs( tabbedPane1, moreTabs );
-	}
-
-	private void addRemoveMoreTabs( JTabbedPane tabbedPane, boolean add ) {
-		if( add ) {
-			tabbedPane.addTab( "Tab 4", new JLabel( "tab 4" ) );
-			tabbedPane.addTab( "Tab 5", new JLabel( "tab 5" ) );
-		} else {
-			int tabCount = tabbedPane.getTabCount();
-			if( tabCount > 3 ) {
-				for( int i = 0; i < 2; i++ )
-					tabbedPane.removeTabAt( tabbedPane.getTabCount() - 1 );
-			}
-		}
-	}
-
-	private void showJidePopupButtonActionPerformed( ActionEvent e ) {
-		Component invoker = (Component) e.getSource();
-
-		JPanel panel = new JPanel( new MigLayout() );
-		panel.add( new JLabel( "Name:") );
-		panel.add( new JTextField( 20 ) );
-
-		JidePopup popupMenu = new JidePopup();
-		popupMenu.add( panel );
-		popupMenu.setDetached( true );
-		popupMenu.setOwner( invoker );
-		popupMenu.showPopup();
+	private void paintTicks() {
+		horizontalRangeSlider.setPaintTicks( paintTick.isSelected() );
+		verticalRangeSlider.setPaintTicks( paintTick.isSelected() );
 	}
 
 	private void initComponents() {
@@ -91,26 +57,19 @@ public class FlatRangeSliderTest
         JPanel panel9 = new JPanel();
         JLabel tabbedPaneLabel = new JLabel();
         JLabel label1 = new JLabel();
-        RangeSlider horizontalRangeSlider = new RangeSlider();
+        horizontalRangeSlider = new RangeSlider();
         JLabel label2 = new JLabel();
-        RangeSlider verticalRangeSlider = new RangeSlider();
+        verticalRangeSlider = new RangeSlider();
         JPanel panel14 = new JPanel();
-        moreTabsCheckBox = new JCheckBox();
-        tabScrollCheckBox = new JCheckBox();
-        hasFullBorderCheckBox = new JCheckBox();
-        JPanel panel10 = new JPanel();
-        JLabel jidePopupLabel = new JLabel();
-        JButton showJidePopupButton = new JButton();
+        paintTick = new JCheckBox();
+        paintLabel = new JCheckBox();
 
         //======== this ========
-        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(
-        new javax.swing.border.EmptyBorder(0,0,0,0), "JF\u006frmDes\u0069gner \u0045valua\u0074ion"
-        ,javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM
-        ,new java.awt.Font("D\u0069alog",java.awt.Font.BOLD,12)
-        ,java.awt.Color.red), getBorder())); addPropertyChangeListener(
-        new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e
-        ){if("\u0062order".equals(e.getPropertyName()))throw new RuntimeException()
-        ;}});
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0
+        ,0,0,0), "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM
+        ,new java.awt.Font("D\u0069al\u006fg",java.awt.Font.BOLD,12),java.awt.Color.red),
+         getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e
+        ){if("\u0062or\u0064er".equals(e.getPropertyName()))throw new RuntimeException();}});
         setLayout(new MigLayout(
             "insets dialog,hidemode 3",
             // columns
@@ -123,7 +82,7 @@ public class FlatRangeSliderTest
             panel9.setOpaque(false);
             panel9.setLayout(new FormLayout(
                 "70dlu:grow, $lcgap, 70dlu:grow",
-                "pref, 2*($lgap, fill:70dlu:grow), $lgap, pref, 2*($lgap, default)"));
+                "pref, 2*($lgap, fill:70dlu:grow), $lgap, pref"));
 
             //---- tabbedPaneLabel ----
             tabbedPaneLabel.setText("RangeSlider:");
@@ -151,72 +110,49 @@ public class FlatRangeSliderTest
                     // rows
                     "[center]"));
 
-                //---- moreTabsCheckBox ----
-                moreTabsCheckBox.setText("more tabs");
-                moreTabsCheckBox.setMnemonic('M');
-                moreTabsCheckBox.addActionListener(e -> moreTabsChanged());
-                panel14.add(moreTabsCheckBox, "cell 0 0");
+                //---- paintTick ----
+                paintTick.setText("PaintTicks");
+                paintTick.setMnemonic('M');
+                paintTick.addActionListener(e -> paintTicks());
+                panel14.add(paintTick, "cell 0 0");
 
-                //---- tabScrollCheckBox ----
-                tabScrollCheckBox.setText("tabLayoutPolicy = SCROLL");
-                tabScrollCheckBox.setMnemonic('S');
-                tabScrollCheckBox.setSelected(true);
-                tabScrollCheckBox.addActionListener(e -> tabScrollChanged());
-                panel14.add(tabScrollCheckBox, "cell 1 0,alignx left,growx 0");
-
-                //---- hasFullBorderCheckBox ----
-                hasFullBorderCheckBox.setText("JTabbedPane.hasFullBorder");
-                hasFullBorderCheckBox.setMnemonic('F');
-                hasFullBorderCheckBox.addActionListener(e -> hasFullBorderChanged());
-                panel14.add(hasFullBorderCheckBox, "cell 2 0,alignx left,growx 0");
+                //---- paintLabel ----
+                paintLabel.setText("PaintLabels");
+                paintLabel.setMnemonic('F');
+                paintLabel.addActionListener(e -> paintLabels());
+                panel14.add(paintLabel, "cell 2 0,alignx left,growx 0");
             }
             panel9.add(panel14, CC.xywh(1, 7, 3, 1));
-
-            //======== panel10 ========
-            {
-                panel10.setLayout(new MigLayout(
-                    "insets 3 0 3 3,hidemode 3",
-                    // columns
-                    "[fill]" +
-                    "[fill]",
-                    // rows
-                    "[]"));
-
-                //---- jidePopupLabel ----
-                jidePopupLabel.setText("JidePopup:");
-                panel10.add(jidePopupLabel, "cell 0 0");
-
-                //---- showJidePopupButton ----
-                showJidePopupButton.setText("show JidePopup");
-                showJidePopupButton.addActionListener(e -> showJidePopupButtonActionPerformed(e));
-                panel10.add(showJidePopupButton, "cell 1 0");
-            }
-            panel9.add(panel10, CC.xy(1, 9));
         }
         add(panel9, "cell 0 0");
 		// JFormDesigner - End of component initialization  //GEN-END:initComponents
 
 		horizontalRangeSlider.setOrientation( SwingConstants.HORIZONTAL );
-		horizontalRangeSlider.setMinimum(0  );
-		horizontalRangeSlider.setMaximum(100  );
+		horizontalRangeSlider.setMinimum( 0 );
+		horizontalRangeSlider.setMaximum( 100 );
 		horizontalRangeSlider.setLowValue( 10 );
 		horizontalRangeSlider.setHighValue( 90 );
+		horizontalRangeSlider.setLabelTable( horizontalRangeSlider.createStandardLabels( 10 ) );
+		horizontalRangeSlider.setMinorTickSpacing( 5 );
+		horizontalRangeSlider.setMajorTickSpacing( 10 );
+		horizontalRangeSlider.setPaintTicks( true );
+		horizontalRangeSlider.setPaintLabels( true );
 
 		verticalRangeSlider.setOrientation( SwingConstants.VERTICAL );
-		verticalRangeSlider.setMinimum(0  );
-		verticalRangeSlider.setMaximum(100  );
+		verticalRangeSlider.setMinimum( 0 );
+		verticalRangeSlider.setMaximum( 100 );
 		verticalRangeSlider.setLowValue( 10 );
 		verticalRangeSlider.setHighValue( 90 );
-	}
-
-	private void tabScrollChanged() {
-
+		verticalRangeSlider.setLabelTable( horizontalRangeSlider.createStandardLabels( 10 ) );
+		verticalRangeSlider.setMinorTickSpacing( 5 );
+		verticalRangeSlider.setMajorTickSpacing( 10 );
+		verticalRangeSlider.setPaintTicks( true );
+		verticalRangeSlider.setPaintLabels( true );
 	}
 
 	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - unknown
-    private JCheckBox moreTabsCheckBox;
-    private JCheckBox tabScrollCheckBox;
-    private JCheckBox hasFullBorderCheckBox;
+    private JCheckBox paintTick;
+    private JCheckBox paintLabel;
 	// JFormDesigner - End of variables declaration  //GEN-END:variables
 }
