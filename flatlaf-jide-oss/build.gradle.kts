511dd02107c78d1bfd7191b76af1516de289702c
Karl Tauber <karl@jformdesigner.com>
2021-06-29 22:58:42 +0200

    JIDE: build using latest version of JIDE library `com.formdev:jide-oss:3.7.12`

--- flatlaf-jide-oss/build.gradle.kts
+++ flatlaf-jide-oss/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 	implementation( project( ":flatlaf-core" ) )
 
 	// use compileOnly() because there are various JIDE libraries available on Maven Central
-	compileOnly( "com.formdev:jide-oss:3.7.11.1" )
+	compileOnly( "com.formdev:jide-oss:3.7.12" )
 }
 
 java {
