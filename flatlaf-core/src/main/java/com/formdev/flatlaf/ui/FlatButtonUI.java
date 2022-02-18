c7fa475128f09cd2c94ed7c9448585dafa9ba295
Nicolas Roduit <nicolas.roduit@gmail.com>
2022-02-18 18:30:24 +0100

    NPE when painting icon on OS X top menu bar #483

--- flatlaf-core/src/main/java/com/formdev/flatlaf/ui/FlatButtonUI.java
+++ flatlaf-core/src/main/java/com/formdev/flatlaf/ui/FlatButtonUI.java
@@ -573,7 +573,7 @@ public class FlatButtonUI
 	public static Color buttonStateColor( Component c, Color enabledColor, Color disabledColor,
 		Color focusedColor, Color hoverColor, Color pressedColor )
 	{
-		if( !c.isEnabled() )
+		if(c != null && !c.isEnabled() )
 			return disabledColor;
 
 		if( c instanceof AbstractButton ) {
@@ -586,7 +586,7 @@ public class FlatButtonUI
 				return hoverColor;
 		}
 
-		if( focusedColor != null && isFocusPainted( c ) && FlatUIUtils.isPermanentFocusOwner( c ) )
+		if( c != null && focusedColor != null && isFocusPainted( c ) && FlatUIUtils.isPermanentFocusOwner( c ) )
 			return focusedColor;
 
 		return enabledColor;
